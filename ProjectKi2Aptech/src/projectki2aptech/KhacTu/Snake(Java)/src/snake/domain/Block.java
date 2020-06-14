b15918dcc9b7a0b093dc782dedd8c60e40e6b5ca
khactu214 <44285970+khactu214@users.noreply.github.com>
2020-06-14 15:16:41 +0700

    game

---
+++ ProjectKi2Aptech/src/projectki2aptech/KhacTu/Snake(Java)/src/snake/domain/Block.java
@@ -0,0 +1,13 @@
+package snake.domain;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Block extends Sprite
+{
+	@Override
+	public void paint(Graphics2D g2) {
+		g2.setColor(Color.green);
+		g2.fillRect(x, y, width, height);
+	}
+}
---
+++ ProjectKi2Aptech/src/projectki2aptech/KhacTu/Snake(Java)/src/snake/domain/Drawable.java
@@ -0,0 +1,7 @@
+package snake.domain;
+
+import java.awt.Graphics2D;
+
+public interface Drawable {
+	public abstract void paint(Graphics2D g2);
+}
